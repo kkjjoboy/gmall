33d28f8a55f8e06e0b8986f3c62df9cd6768e2bd
kkjjoboy <2217023985@qq.com>
2020-03-02 18:43:20 +0800

    配置通用mapper、编写测试类

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,4 +1,38 @@
 package com.atguigu.gmall.user.controller;
 
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser() {
+
+        List<UmsMember> umsMemberList = userService.getAllUser();
+        return umsMemberList;
+    }
+
+    @RequestMapping("getUmsMemberReceiveAddress")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getUmsMemberReceiveAddress(String memberId) {
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddressList = userService.getUmsMemberReceiveAddress(memberId);
+        return umsMemberReceiveAddressList;
+    }
+    @RequestMapping("index")
+    @ResponseBody
+    public String index() {
+        return "hello user";
+    }
 }
